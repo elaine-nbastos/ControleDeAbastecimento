5a0e17003e289f8d5c00f38f41451002240e180e
Elaine Bastos <elaineb80@hotmail.com>
2024-04-28 20:21:41 -0300

    Primeiro commit

--- script.js
+++ script.js
@@ -6,10 +6,42 @@ inputCheck.addEventListener('click', ()=>{
     elemento.setAttribute('data-bs-theme', modo)
 })
 
+function calcular() {
+    let litros = parseFloat(document.getElementById('litros').value);
+    let valor = parseFloat(document.getElementById('valor').value);
+    let kmRodado = parseFloat(document.getElementById('kmRodado').value);
+    let feitoNoDia = parseFloat(document.getElementById('feitoNoDia').value);
+    
+    let totalDoLitro = valor / litros;
+    let totalKmPorLitro = kmRodado / litros;
+    let totalValorPorKm = valor / kmRodado;
+    let totalLucroDoDia = feitoNoDia - valor;
 
-function valorDoLitro(){
-    const valorDoLitro = parseInt(document.querySelector('#valorDoLitro').value);
-}
+    document.getElementById('valorDoLitro').innerHTML = totalDoLitro;
+    document.getElementById('kmPorLitro').innerHTML = totalKmPorLitro;
+    document.getElementById('valorDoKm').innerHTML = totalValorPorKm;
+    document.getElementById('lucroDoDia').innerHTML = totalLucroDoDia;
+  
+    // Salvando os dados no localStorage
+    localStorage.setItem('litros', litros);
+    localStorage.setItem('valor', valor);
+    localStorage.setItem('kmRodado', kmRodado);
+    localStorage.setItem('feitoNoDia', feitoNoDia);
 
-
-
+  }
+  
+  // Carregando os valores salvos no localStorage ao carregar a página
+  window.onload = function() {
+    let litros = localStorage.getItem('litros');
+    let valor = localStorage.getItem('valor');
+    let kmRodado = localStorage.getItem('kmRodado');
+    let feitoNoDia = localStorage.getItem('feitoNoDia');
+  
+    if (litros && valor && kmRodado) {
+      document.getElementById('litros').value = litros;
+      document.getElementById('valor').value = valor;
+      document.getElementById('kmRodado').value = kmRodado;
+      document.getElementById('feitoNoDia').value = feitoNoDia;
+    }
+  };
+  
