20659cee9b679a6011880d14ee8ca6473e45d7fb
Elaine Bastos <elaineb80@hotmail.com>
2024-04-29 17:17:54 -0300

    Feito a responsividade da pagina e inserido o campo de qtd de corridas do dia

--- script.js
+++ script.js
@@ -11,37 +11,43 @@ function calcular() {
     let valor = parseFloat(document.getElementById('valor').value);
     let kmRodado = parseFloat(document.getElementById('kmRodado').value);
     let feitoNoDia = parseFloat(document.getElementById('feitoNoDia').value);
+    let corridasFeitasNoDi = parseFloat(document.getElementById('corridasFeitasNoDi').value);
     
     let totalDoLitro = valor / litros;
     let totalKmPorLitro = kmRodado / litros;
     let totalValorPorKm = valor / kmRodado;
     let totalLucroDoDia = feitoNoDia - valor;
+    let totalValorDaCorrida = feitoNoDia / corridasFeitasNoDi
 
-    document.getElementById('valorDoLitro').innerHTML = totalDoLitro;
-    document.getElementById('kmPorLitro').innerHTML = totalKmPorLitro;
-    document.getElementById('valorDoKm').innerHTML = totalValorPorKm;
-    document.getElementById('lucroDoDia').innerHTML = totalLucroDoDia;
+    document.getElementById('valorDoLitro').innerHTML = `R$ ${totalDoLitro}`;
+    document.getElementById('kmPorLitro').innerHTML = `${totalKmPorLitro}km`;
+    document.getElementById('valorDoKm').innerHTML = `R$${totalValorPorKm} centavos`;
+    document.getElementById('lucroDoDia').innerHTML = `R$${totalLucroDoDia}`;
+    document.getElementById('valorDaCorrida').innerHTML = `R$${totalValorDaCorrida}`;
   
     // Salvando os dados no localStorage
     localStorage.setItem('litros', litros);
     localStorage.setItem('valor', valor);
     localStorage.setItem('kmRodado', kmRodado);
     localStorage.setItem('feitoNoDia', feitoNoDia);
+    localStorage.setItem('corridasFeitasNoDia', corridasFeitasNoDia);
 
   }
-  
   // Carregando os valores salvos no localStorage ao carregar a página
   window.onload = function() {
     let litros = localStorage.getItem('litros');
     let valor = localStorage.getItem('valor');
     let kmRodado = localStorage.getItem('kmRodado');
     let feitoNoDia = localStorage.getItem('feitoNoDia');
+    let corridasFeitasNoDia = localStorage.getItem('corridasFeitasNoDia');
   
-    if (litros && valor && kmRodado) {
+    if (litros && valor && kmRodado && feitoNoDia && corridasFeitasNoDia) {
       document.getElementById('litros').value = litros;
       document.getElementById('valor').value = valor;
       document.getElementById('kmRodado').value = kmRodado;
       document.getElementById('feitoNoDia').value = feitoNoDia;
+      document.getElementById('corridasFeitasNoDia').value = corridasFeitasNoDia;
+      
     }
   };
   
