d98a88f5afdf106c4c5f8da4fc11e8cb8988ea4f
Elaine Bastos <elaineb80@hotmail.com>
2024-04-30 20:15:49 -0300

    inicio do refatoramento do arquivo com TypeScript.

--- js/bytebank.js
+++ js/bytebank.js
@@ -1,41 +1,37 @@
-let saldo = 1;
-
-const elementoSaldo = document.querySelector('.saldo-valor .valor');
-elementoSaldo.textContent = saldo;
-
-const elementoFormulario = document.querySelector('.block-nova-transacao form');
-elementoFormulario.addEventListener('submit', function(event){
+var saldo = 1;
+var elementoSaldo = document.querySelector('.saldo-valor .valor');
+if (elementoSaldo !== null) {
+    elementoSaldo.textContent = saldo.toString();
+}
+var elementoFormulario = document.querySelector('.block-nova-transacao form');
+elementoFormulario.addEventListener('submit', function (event) {
     event.preventDefault();
-    if(!elementoFormulario.checkValidity()){
+    if (!elementoFormulario.checkValidity()) {
         alert("Por favor, preencha todos os campos da transação!");
         return;
     }
-
-    const inputTipoTransacao = elementoFormulario.querySelector('#tipoTransacao');
-    const inputValor = elementoFormulario.querySelector('#valor');
-    const inputData = elementoFormulario.querySelector('#data');
-
-    let tipoTransacao = inputTipoTransacao.value;
-    let valor = inputValor.value;
-    let data = inputData.value;
-
+    var inputTipoTransacao = elementoFormulario.querySelector('#tipoTransacao');
+    var inputValor = elementoFormulario.querySelector('#valor');
+    var inputData = elementoFormulario.querySelector('#data');
+    var tipoTransacao = inputTipoTransacao.value;
+    var valor = inputValor.valueAsNumber;
+    var data = new Date(inputData.value);
     if (tipoTransacao == "Ganhos") {
         saldo += valor;
-    }else if(tipoTransacao == "Abastecimento" || tipoTransacao == "Manutenção do veículo") {
+    }
+    else if (tipoTransacao == "Abastecimento" || tipoTransacao == "Manutenção do veículo") {
         saldo -= valor;
-    }else {
+    }
+    else {
         alert("Tipo de transação é invalido!");
         return;
     }
-
-    elementoSaldo.textContent = saldo;
-
-    const novaTransacao = {
+    elementoSaldo.textContent = saldo.toString();
+    var novaTransacao = {
         tipoTransacao: tipoTransacao,
         valor: valor,
         data: data
-    }
-
+    };
     console.log(novaTransacao);
     elementoFormulario.reset();
 });
